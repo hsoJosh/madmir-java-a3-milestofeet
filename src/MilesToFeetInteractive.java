fd2c1db260d605008bb76df4a82f1a76dee502f3
hsoJosh <joshua_eh@hotmail.com>
2013-08-12 13:52:35 -0400

    Thanks for the help Clyde! It really cleared up some frustrations in my head.

--- src/MilesToFeetInteractive.java
+++ src/MilesToFeetInteractive.java
@@ -1,12 +1,31 @@
+import java.util.Scanner;
+
+import javax.swing.JOptionPane;
 
 public class MilesToFeetInteractive {
-
-	public static void main(String[] args) {
-		// -------INSTRUCTIONS-------
-		// 1. Refer to exercise 4 on page 113 of the Joyce Farrell book (chapter 2 PDF)
-		// 2. In this file, complete part (b) of this exercise
-		// 3. For this class, you WILL receive an input from the user.
-
+		
+		
+		
+		
+		
+		public static void main(String[] args) {
+			//Declare and initialize named constant FEET_PER_MILE
+			final int FEET_PER_MILE = 5280;
+			
+			//Declare and initialize variables
+			float uncleMiles, uncleFeet;
+			Scanner kbrd = new Scanner(System.in);
+			//User Input
+			System.out.println("What is the distance in miles to your uncles house.");
+			uncleMiles = kbrd.nextFloat();
+			
+			//Calculations
+			uncleFeet = uncleMiles * FEET_PER_MILE;
+			
+			//DisplayOutPut
+			JOptionPane.showMessageDialog(null, "The Distance to my uncles house is " + uncleMiles + " miles. That's " + uncleFeet + " feet!");
+			
+			
 	}
 
 }
